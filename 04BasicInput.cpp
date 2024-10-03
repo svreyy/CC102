1b6ca733f47de34c4ebc69098e0218dd0b4c00dd
Reychelle Pajarillo <pajarillochelle188@gmail.com>
2024-10-03 13:01:11 +0800

    Update 04BasicInput.cpp

--- 04BasicInput.cpp
+++ 04BasicInput.cpp
@@ -6,14 +6,14 @@ int main ()
 {
 	string  nickname;
 	string course;
-  string school;
+        string school;
 
-		cout << " Nickname: ";
+	cout << " Nickname: ";
   	cin >> nickname ;
   	cout << "Course - Year Level: " ;
   	cin >> course;
   	cout << "School:" ;
-    cin >> school ;
+        cin >> school ;
   	cout << "Wow Congrats " << nickname << "! " << course << " is a nice course. And you are studying in " << school<< " which is one of the Center Excellence in Tertiary Education." ;
  return 0;
 
