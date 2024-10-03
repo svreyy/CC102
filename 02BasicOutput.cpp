4cfb17d7152e1a396befbde29f131ebc18be432f
Reychelle Pajarillo <pajarillochelle188@gmail.com>
2024-10-03 10:27:39 +0800

    Add files via upload

---
+++ 02BasicOutput.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{ 
+    cout <<"Our Lady of Lourdes College Foundation"<<endl;
+	cout <<"College of Computer Studies"<<endl;
+	cout <<"Bachelor of Science in Information Technology-1st Year"<<endl;
+	cout <<"Reychelle Joy Pajarillo"<<endl;
+	cout <<"Purok-3,Lico 1 Sto. Domingo, Vinzons, Camarines Norte"<<endl;
+	return 0;
+}
