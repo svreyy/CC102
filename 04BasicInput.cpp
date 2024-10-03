8edab0b9f593c4e2cfa11ea80bbe92318be63aad
Reychelle Pajarillo <pajarillochelle188@gmail.com>
2024-10-03 13:00:34 +0800

    Update 04BasicInput.cpp

--- 04BasicInput.cpp
+++ 04BasicInput.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
+
 using namespace std;
-int main()
+
+int main ()
 {
-string nickname, school, cyl;
-  cout<<"Nickname:";
-  cin>>nickname;
-  cout<<"Course-Year Level:";
-  cin>>cyl;
-  cout<<"School:";
-  cin>>school;
-  cout << "Wow, congrats " << nickname << "! " << courseYear << " is a nice course." << endl;
-  cout << "And you are studying at " << school << ", which is one of the Centers of Excellence in Tertiary Education.";
-return 0;
-  
+	string  nickname;
+	string course;
+  string school;
+
+		cout << " Nickname: ";
+  	cin >> nickname ;
+  	cout << "Course - Year Level: " ;
+  	cin >> course;
+  	cout << "School:" ;
+    cin >> school ;
+  	cout << "Wow Congrats " << nickname << "! " << course << " is a nice course. And you are studying in " << school<< " which is one of the Center Excellence in Tertiary Education." ;
+ return 0;
+
 }
