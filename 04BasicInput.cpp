1d2a33cfab7037cc82d858f698a8bfdc2f779434
Reychelle Pajarillo <pajarillochelle188@gmail.com>
2024-10-03 11:32:02 +0800

    Update 04BasicInput.cpp

--- 04BasicInput.cpp
+++ 04BasicInput.cpp
@@ -3,11 +3,12 @@ using namespace std;
 int main()
 {
 string nickname, school, cyl;
-cout<<"Nickname:";
-cin>>nickname;
-cout<<"Course-Year Level:";
-cin>>cyl;
-cout<<"School:";
-cin>>school;
+  cout<<"Nickname:";
+  cin>>nickname;
+  cout<<"Course-Year Level:";
+  cin>>cyl;
+  cout<<"School:";
+  cin>>school;
 return 0;
+  
 }
