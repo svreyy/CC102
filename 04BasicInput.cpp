47e03be45a43b6647294794f43f721954d0ce5ca
Reychelle Pajarillo <pajarillochelle188@gmail.com>
2024-10-03 11:25:50 +0800

    Add files via upload

---
+++ 04BasicInput.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+string nickname, school, cyl;
+cout<<"Nickname:";
+cin>>nickname;
+cout<<"Course-Year Level:";
+cin>>cyl;
+cout<<"School:";
+cin>>school;
+return 0;
+}
