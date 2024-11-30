dfd40e345b3f6cdb5833b837a348b2362b0bb653
Reychelle Pajarillo <pajarillochelle188@gmail.com>
2024-11-30 10:42:39 +0800

    Update Activity01Looping_C.cpp

--- Activity01Looping_C.cpp
+++ Activity01Looping_C.cpp
@@ -8,18 +8,17 @@ int main()
      
      for (int i = 0; i < n; ++i) {
        
-           for (int j = 0; j < i; ++j) {
-                 cout << "  ";
+         for (int j = 0; j < i; ++j) {
+             cout << "  ";
                  
-           }
+         }
            
-           for (int k = 0; k < n - i; ++k) {
-                 cout << "# ";
+         for (int k = 0; k < n - i; ++k) {
+             cout << "# ";
                  
-          }
-          
+         }
          cout << endl;
-    }
+     }
     
-    return 0;
+     return 0;
 }
