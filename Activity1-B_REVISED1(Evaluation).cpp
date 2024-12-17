3858ad68ba10eb51f0acee7806e56d81f6dc8580
Reychelle Pajarillo <pajarillochelle188@gmail.com>
2024-12-17 15:03:50 +0800

    Add files via upload

---
+++ Activity1-B_REVISED1(Evaluation).cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+
+	int n = 7;
+	for(int i = 0; i < n; ++i){
+		for(int j = 0; j < i; ++j){
+			cout << " ";
+		}
+	for(int k=0; k<n-i;++k){
+		if((i+k)%2==0){
+		    cout << "#";
+		}
+		else{
+		    cout<< "@";
+		}
+	}
+	cout <<endl;
+	}
+	
+	return 0;
+}
