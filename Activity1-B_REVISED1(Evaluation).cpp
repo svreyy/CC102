e66634707593847925a16507a6729cc4d7ac9106
Reychelle Pajarillo <pajarillochelle188@gmail.com>
2024-12-17 15:27:01 +0800

    Update Activity1-B_REVISED1(Evaluation).cpp

--- Activity1-B_REVISED1(Evaluation).cpp
+++ Activity1-B_REVISED1(Evaluation).cpp
@@ -6,14 +6,14 @@ int main (){
 	int n = 7;
 	for (int i = 0; i < n; ++i) {
 		for (int j = 0; j < i; ++j) {
-			cout << " ";
+			cout << "  ";
 		}
 	for (int k=0; k < n-i; ++k) {
 		if ((i+k) % 2 == 0) {
-		    cout << "#";
+		    cout << "# ";
 		}
 		else {
-		    cout << "@";
+		    cout << "@ ";
 		}
 	}
 	cout << endl;
